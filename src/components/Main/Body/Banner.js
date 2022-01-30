fbc2babc5c93fe186f58eb94b7cc31db85351bb6
sadrulj <jamil.sadrul@gmail.com>
2022-01-30 10:54:21 +0600

    banner fixed

--- src/components/Main/Body/Banner.js
+++ src/components/Main/Body/Banner.js
@@ -10,11 +10,13 @@ const Banner = () => {
         </p>
         <a href="#">Learn More</a>
       </div>
-      <img
-        className="deliveryPic"
-        src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdelivery.png?alt=media&token=69b9823d-96df-452a-bd4e-14d27a4cc337"
-        alt=""
-      />
+      <div className="">
+        <img
+          className="deliveryPic"
+          src="https://firebasestorage.googleapis.com/v0/b/food-delivery-37c59.appspot.com/o/Images%2Fdelivery.png?alt=media&token=69b9823d-96df-452a-bd4e-14d27a4cc337"
+          alt=""
+        />
+      </div>
     </div>
   );
 };
